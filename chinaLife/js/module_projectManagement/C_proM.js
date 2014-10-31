2ef72f46a991da6cb9a8c47a2e18f4b2de06acf3
xiaolongXL <rainbolean@sina.com>
2014-10-31 16:17:39 +0800

    重构代码

---
+++ chinaLife/js/module_projectManagement/C_proM.js
@@ -0,0 +1,15 @@
+define(function (require,exports,module){
+
+	/**
+	* 依赖模块
+	**/
+	var common = require('module_common/common');
+
+	function create_List(){
+
+	};
+
+	module.exports = {
+		create_List:create_List
+	};
+});
--- chinaLife/js/module_projectManagement/main.js
+++ chinaLife/js/module_projectManagement/main.js
@@ -4,6 +4,7 @@ define(function (require,exports,module){
 	* 依赖模块
 	**/
 	var common = require('module_common/common');
+	var C_proM = require('module_projectManagement/C_proM');
 
 	/**
 	 * 根据url的hash值来确定当前处在那个模块，以此来确定给导航上得按钮添加高亮效果
@@ -24,5 +25,10 @@ define(function (require,exports,module){
 	* 生成用户名
 	**/
 	common.valuationUsername();
+
+	/**
+	* 项目列表
+	**/
+	C_proM.create_List();
 
 });
